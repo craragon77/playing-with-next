ac897c2cd4defd8159e0924f8b6cd87b99062e5a
craragon77 <aragonchristopherray@gmail.com>
2024-08-20 20:48:40 -0400

    pushing everything

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,20 +18,29 @@ export default function Home() {
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="quarter">
-          <img src="/AragonChris.jpg" alt="test"/>
-          <h1>Hi! I'm Chris, a web and GIS developer based on New England</h1>
+      <main>
+        <div className="quarter black">
+          <img src="/fotos/AragonChris.jpg" alt="test" className="myFace"/>
+          <h1>Hi! I'm Chris, an award-winning web and GIS developer and speaker based on New England</h1>
+          <ul className="contact-info">
+            <li><a href="https://www.linkedin.com/in/christopher-aragon/" target="_blank">LinkedIn</a></li>
+            <li><a href="https://github.com/craragon77" target="_blank"/>Github</li>
+            <li><a href="https://www.stone-env.com/who-we-are/our-team/chris-aragon/"/>Company</li>
+            <li>Resume</li>
+          </ul>
         </div>
-        <div className="quarter">
-          <p>I see programming, data structures, and algorithms as a powerful, dynamic, and quasi-magical tool. Having worked as a software developer in the education, healthcare, and sustainability fields,  I am deeply interested in the interdisciplinary applications of programming being used in creative and fascinating ways to solve complex challenges. Feel free to reach out! I love sharing my work and I love learning what other developers and engineers are working on too! Outside of work, I like hiking, cycling, and playing music.</p>
+        <div className="quarter white">
+          <p>
+            I see programming, data structures, and algorithms as a powerful, dynamic, and quasi-magical tool. <br/>
+            Having worked as a software developer in the education, healthcare, and sustainability fields,  I know the interdisciplinary applications of programming and how to solve complex problems. <br/>
+            Feel free to reach out! I love sharing my work and I love learning what other developers and engineers are working on too! Outside of work, I like hiking, cycling, and playing music.</p>
         </div>
-        <div className="quarter">
+        <div className="quarter black">
           <h2>Portfolio</h2>
           {mapProjects()}
         </div>
       </main>
-      <footer>
+      {/* <footer className="white">
         <div>
           <p>Feel free to reach out!</p>
           <ul>
@@ -39,7 +48,7 @@ export default function Home() {
             <li><a href="https://github.com/craragon77" target="_blank"/>Github</li>
           </ul>
         </div>
-      </footer>
+      </footer> */}
     </>
   );
 }
