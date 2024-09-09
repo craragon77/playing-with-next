340a4ef0a34cc236e65725857861b38b12c45139
craragon77 <aragonchristopherray@gmail.com>
2024-09-09 19:15:23 -0400

    stashing this

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,27 @@
 import Image from "next/image";
 import projects from './projects'; 
-import headshot from "./fotos/AragonChris.jpg";
-import test from "./fotos/FFI1.png";
+import Headshot from "/fotos/AragonChris.jpg";
+import FFI1 from "./fotos/FFI1.png";
+import FFI2 from "./fotos/FFI2.png";
+import FFI3 from "./fotos/FFI3.png";
+import FFI4 from "./fotos/FFI4.png";
+import FFI5 from "./fotos/FFI5.png";
+
 
 export default function Home() {
 
   function mapProjects(){
     return projects.map((projects, idx) => {
       //TODO - add photo mapping from project.js
-      return <div>
+      return <div id={`mapped-projects-${idx}`}>
         <p>Name: {projects.name}</p><br/>
         <p>Description: {projects.description}</p>
         <p>url to live site: <a target="_blank" href={projects.url}>{projects.url}</a></p>
+        <div className="project-image-container">
+          {projects.images.map((photo, localIdx) => {
+            return <img src={`/fotos/${photo}`} alt={`image of ${photo} number ${localIdx + 1}`}/>
+          })}
+        </div>
       </div>
     });
   }
@@ -21,7 +31,9 @@ export default function Home() {
       <main>
         <div className="quarter black">
           <img src="/fotos/AragonChris.jpg" alt="test" className="myFace"/>
-          <h1>Hi! I'm Chris, an award-winning web and GIS developer and speaker based on New England</h1>
+          {/* <img src={`${Headshot}`} alt="test" className="myFace"/> */}
+          <h1>Chris Aragon</h1>
+          <h3>Award-winning Web and GIS Developer</h3>
           <ul className="contact-info">
             <li><a href="https://www.linkedin.com/in/christopher-aragon/" target="_blank">LinkedIn</a></li>
             <li><a href="https://github.com/craragon77" target="_blank"/>Github</li>
@@ -29,12 +41,12 @@ export default function Home() {
             <li>Resume</li>
           </ul>
         </div>
-        <div className="quarter white">
+        {/* <div className="quarter white">
           <p>
             I see programming, data structures, and algorithms as a powerful, dynamic, and quasi-magical tool. <br/>
             Having worked as a software developer in the education, healthcare, and sustainability fields,  I know the interdisciplinary applications of programming and how to solve complex problems. <br/>
             Feel free to reach out! I love sharing my work and I love learning what other developers and engineers are working on too! Outside of work, I like hiking, cycling, and playing music.</p>
-        </div>
+        </div> */}
         <div className="quarter black">
           <h2>Portfolio</h2>
           {mapProjects()}
--- src/app/projects.js
+++ src/app/projects.js
@@ -1,28 +1,28 @@
 let projects = [
     {
-        "name": "Functional Floodplain Initiative (FFI')",
+        "name": "Functional Floodplain Initiative (FFI)",
         "description": "Functional Floodplain Initiative is a web application for visualizing and studying bodies of water from across Vermont. Commissioned by the Vermont Department of Environmental, it’s a program for scientists and the public to visualize, study, and extract data from watersheds across the state in order to protect and preserve the wilderness.",
         "url": "https://ffi.stone-env.net/home",
-        "images": ["./fotos.FFI1.png", "./fotos.FFI2.png", "./fotos.FFI3.png", "./fotos.FFI4.png", "./fotos.FFI5.png"]
+        "images": ["FFI1.png", "FFI2.png", "FFI3.png", "FFI4.png", "FFI5.png"]
     },
     {
         "name": "Conservation Outcomes Research Explorer (CORE)",
         "description": "Conservation Outcomes Research Explorer is a web application to collect, compare, contrast, and visualize hundreds of thousands of different studies about soil ecology and land management. Commissioned by the United States Department of Agriculture, it’s a tool to use geospatial tools and data visualization to let researchers more quickly find academic studies organized by keyword and coordinate.",
         "url": "https://nrcs-core-dev.stone-env.net/home",
-        "images": []
+        "images": ["CORE1.png", "CORE2.png", "CORE3.png","CORE4.png"]
     },
     {
         "name": "The New York Gateway",
         "description": "New York Gateway is an award-winning GIS application for bringing together science, recreation, and stories from across the State of New York. Commissioned by the New York Office of Planning, Development, and Community Infrastructure, it is an application built on public GIS maps and state geospatial resources to teach the general public about ecology and history, promote tourism, and guide sustainable stewardship of the State’s environment.",
         "url": "https://new-york-opd-geographic-information-gateway-nysdos.hub.arcgis.com/",
-        "images": []
+        "images": ["NYGateway1.png", "NYGateway2.png", "NYGateway3.png", "NYGateway4.png", "NYGateway5.png"]
     },
-    {
-        "name": "New York GeoPossibilities Web Portal",
-        "description": "New York Geopossibilities is a simple web application built for property-owners to see how much they can save by switching their heating from fossil fuels to a heat-pump. Commissioned by the New York State Energy Research and Development Authority, it’s a tool for people to get real-time estimates of their savings when switching to renewable heating sources based on their building type, zip code, and geography.",
-        "url": "https://new-york-opd-geographic-information-gateway-nysdos.hub.arcgis.com/",
-        "images": []
-    }
+    // {
+    //     "name": "New York GeoPossibilities Web Portal",
+    //     "description": "New York Geopossibilities is a simple web application built for property-owners to see how much they can save by switching their heating from fossil fuels to a heat-pump. Commissioned by the New York State Energy Research and Development Authority, it’s a tool for people to get real-time estimates of their savings when switching to renewable heating sources based on their building type, zip code, and geography.",
+    //     "url": "https://new-york-opd-geographic-information-gateway-nysdos.hub.arcgis.com/",
+    //     "images": []
+    // }
 ];
 
 export default projects;
